a78fbcb68e1a84b257eebdc44efec465687e301d
Francisco Ramos <franramos@outlook.com>
2021-09-26 20:20:16 +0200

    Ingress routes

--- posts/index.js
+++ posts/index.js
@@ -17,7 +17,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   console.log(`POST /posts`, req.body);
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
